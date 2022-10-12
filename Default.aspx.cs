f314b3921a148e7635029b749f6b7cbb002c5580
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-10-12 23:12:18 +0300

    Check winner-draw situations

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.ServiceModel.Channels;
 using System.Web;
@@ -55,6 +56,52 @@ public partial class _Default : Page
 
         checkWinner();
     }
-    
-    
+    private void checkWinner()
+    {
+        bool there_is_a_winner = false;
+
+        //Horizontal check
+        if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            there_is_a_winner = true;
+        if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            there_is_a_winner = true;
+        if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            there_is_a_winner = true;
+        //Vertical check
+        if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            there_is_a_winner = true;
+        if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            there_is_a_winner = true;
+        if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            there_is_a_winner = true;
+
+        if (there_is_a_winner)
+        {
+            disableButtons();
+            String winner = "";
+            if (turn)
+                winner = "0";
+            else
+                winner = "X";
+            TextBox2.Text = winner + " Win!!!";
+        }//end if
+        else
+        {
+            if (counter == 9)
+                TextBox2.Text = "Draw!!!";
+        }
+    }
+    private void disableButtons()
+    {
+        try
+        {
+            foreach (Control c in Controls)
+            {
+                Button b = (Button)c;
+                b.Enabled = false;
+            }
+        }
+        catch { }
+    }
+
 }
